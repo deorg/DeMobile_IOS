ea36522dab8da517af2c07521c7819a67e79d230
chaiprapun <chaiprapun@hotmail.com>
2019-03-05 22:01:43 +0700

    - first commit

---
+++ DmobileApp/DmobileApp/Fistpage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DmobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Fistpage : ContentPage
+	{
+		public Fistpage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello world", "OK");
+        }
+    }
+}
